040a97fcea37fb5b1ae45b5531d019890f2fa9d4
tran thanh <trantrungthanh@trans-Mac.local>
2017-05-29 11:25:56 +0700

    Initial Commit

---
+++ CaulatorTip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaulatorTip
+//
+//  Created by tran trung thanh on 5/29/17.
+//  Copyright © 2017 tran trung thanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
